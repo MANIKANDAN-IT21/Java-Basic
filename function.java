eed534e11ddc220149161ef8bb4e5596234a0a3e
MANIKANDAN-IT21 <manikandan.it21@bitsathy.ac.in>
2024-04-23 12:17:36 +0530

    JAVA

---
+++ function.java
@@ -0,0 +1,18 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        //function call
+        mani();
+        System.out.println("manikandan");
+    }
+    //function definition
+    public static void mani(){
+        int a=10;
+        System.out.println(a);
+    }
+}
+// output
+// 10
+// manikandan
+______________________________________________________________
+
