fb6c13173b99876b52cac800c46cdd84474bdd7b
MANIKANDAN-IT21 <manikandan.it21@bitsathy.ac.in>
2024-04-23 20:27:59 +0530

    JAVA

---
+++ Command line arugument.java
@@ -0,0 +1,14 @@
+class command{
+    public static void main(String args[]){
+        System.out.println("Manikandan");
+    }
+}
+// String args==is command line argument!!
+// file la complie and run panum podhu! apo arugrument pass panratu thn command line arugrument
+// epdi pakkararthu? for loop use panee pakkalam!! 
+// normally the argrs will be empty! so nothing will be print if i pass any arugement in complie or run time it will be store in the args array!!
+// For example
+// javac command.java--->compliation 
+// java command mani kandan --->run time
+// mani
+// kandan
