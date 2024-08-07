0b8ce980249efb92647890ffa0ee3028604db094
Manikamaraj <manikandan.it21@bitsathy.ac.in>
2024-08-07 14:37:21 +0530

    BASIC

---
+++ HashMap.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        HashMap<Integer,String> std=new HashMap<>();
+        std.put(1,"mani");
+        std.put(2,"moni");
+        std.put(3,"kani");
+        std.put(4,"sani");
+        System.out.println(std.size());
+    }
+}
